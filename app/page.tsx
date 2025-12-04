839f3095932d4b652c5f261b0e2c6ab37f8fa696
Hashem63YT <Hashem.Alkooheji@gmail.com>
2025-12-04 14:00:49 +0300

    fixing...

--- app/page.tsx
+++ app/page.tsx
@@ -515,7 +515,7 @@ export default function Home() {
                 transition={{ duration: 0.6 }}
               >
                 <h1 className="font-bold font-arabic-display text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-center text-islamic-green-700 mb-1 sm:mb-2 arabic-text">
-                  حملة الصلاة على النبي ﷺ
+                  الموقع تحت التجربة - نرجوا عدم إضافة أعداد جديدة
                 </h1>
               </motion.div>
 
